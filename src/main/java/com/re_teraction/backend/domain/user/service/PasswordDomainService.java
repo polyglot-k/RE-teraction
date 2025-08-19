7aa33b2ef7f21fa1c6a34b171a1dec9fa938aa4e
KNU-K <knukang334@gmail.com>
2025-08-20 01:32:06 +0900

    chore(user): PasswordDomainService 내에 암호화 관련 TODO 추가

--- src/main/java/com/re_teraction/backend/domain/user/service/PasswordDomainService.java
+++ src/main/java/com/re_teraction/backend/domain/user/service/PasswordDomainService.java
@@ -5,6 +5,7 @@ import com.re_teraction.backend.global.annotation.DomainService;
 @DomainService
 public class PasswordDomainService {
     public String encode(String plain){
+        //TODO: 추후 암호화 로직 추가
         return plain;
     }
 }
