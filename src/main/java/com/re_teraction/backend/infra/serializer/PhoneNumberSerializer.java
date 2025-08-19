20a5922f89f895b3f87b34477611fd7a6b916420
KNU-K <knukang334@gmail.com>
2025-08-20 01:06:09 +0900

    feat(user): PhoneNumber Value Object 추가 및 flat Response 형식으로 직렬화

---
+++ src/main/java/com/re_teraction/backend/infra/serializer/PhoneNumberSerializer.java
@@ -0,0 +1,17 @@
+package com.re_teraction.backend.infra.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.re_teraction.backend.domain.user.vo.PhoneNumber;
+import java.io.IOException;
+
+public class PhoneNumberSerializer extends StdSerializer<PhoneNumber> {
+    public PhoneNumberSerializer() { super(PhoneNumber.class); }
+
+    @Override
+    public void serialize(PhoneNumber value, JsonGenerator gen, SerializerProvider provider)
+            throws IOException {
+        gen.writeString(value.getValue());
+    }
+}
