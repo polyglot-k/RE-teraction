e5513f0695309c0b59aab9e2fa960f8767aa5415
KNU-K <knukang334@gmail.com>
2025-08-20 01:09:45 +0900

    feat(user): UserApplicationService 추가 및 사용자 생성 로직 구현

---
+++ src/main/java/com/re_teraction/backend/application/user/UserApplicationService.java
@@ -0,0 +1,36 @@
+package com.re_teraction.backend.application.user;
+
+import com.re_teraction.backend.application.user.dto.CreateUserCommand;
+import com.re_teraction.backend.domain.user.vo.Email;
+import com.re_teraction.backend.domain.user.vo.LoginId;
+import com.re_teraction.backend.domain.user.vo.Password;
+import com.re_teraction.backend.domain.user.vo.PhoneNumber;
+import com.re_teraction.backend.domain.user.entity.UserJpaEntity;
+import com.re_teraction.backend.domain.user.repo.UserJpaRepository;
+import com.re_teraction.backend.domain.user.service.UserMapper;
+import com.re_teraction.backend.global.annotation.ApplicationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+@ApplicationService
+@RequiredArgsConstructor
+@Slf4j
+public class UserApplicationService {
+    private final UserJpaRepository userJpaRepository;
+    private final UserMapper userMapper;
+    @Transactional
+    public Long createUser(CreateUserCommand cmd) {
+        UserJpaEntity user = toEntity(cmd);
+        return userJpaRepository.saveAndHandleDuplicate(user).getId();
+    }
+
+    private UserJpaEntity toEntity(CreateUserCommand cmd) {
+        LoginId loginId = userMapper.toLoginId(cmd.loginId());
+        Email email = userMapper.toEmail(cmd.email());
+        PhoneNumber phoneNumber = userMapper.toPhoneNumber(cmd.phoneNumber());
+        Password password = userMapper.toPassword(cmd.password());
+
+        return UserJpaEntity.of(loginId, password, cmd.name(), email, phoneNumber);
+    }
+}
