d5fb3075058e5c9c30b8da41a7676c8e1cedc447
KNU-K <knukang334@gmail.com>
2025-08-20 01:09:34 +0900

    feat(user): User 생성에 대한 요청 dto 추가

---
+++ src/main/java/com/re_teraction/backend/application/user/dto/CreateUserCommand.java
@@ -0,0 +1,10 @@
+package com.re_teraction.backend.application.user.dto;
+
+public record CreateUserCommand(
+        String loginId,
+        String password,
+        String name,
+        String email,
+        String phoneNumber
+) {
+}
