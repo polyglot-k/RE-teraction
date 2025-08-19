e877b71ef98334d59b9154da72bcc959a8039dbe
KNU-K <knukang334@gmail.com>
2025-08-20 01:20:12 +0900

    fix(user): Serializer 상속 시, 잘못된 제네릭 수정

--- src/main/java/com/re_teraction/backend/infra/serializer/LoginIdSerializer.java
+++ src/main/java/com/re_teraction/backend/infra/serializer/LoginIdSerializer.java
@@ -3,14 +3,14 @@ package com.re_teraction.backend.infra.serializer;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import com.re_teraction.backend.domain.user.vo.Email;
+import com.re_teraction.backend.domain.user.vo.LoginId;
 import java.io.IOException;
 
-public class LoginIdSerializer extends StdSerializer<Email> {
-    public LoginIdSerializer() { super(Email.class); }
+public class LoginIdSerializer extends StdSerializer<LoginId> {
+    public LoginIdSerializer() { super(LoginId.class); }
 
     @Override
-    public void serialize(Email value, JsonGenerator gen, SerializerProvider provider)
+    public void serialize(LoginId value, JsonGenerator gen, SerializerProvider provider)
             throws IOException {
         gen.writeString(value.getValue());
     }
--- src/main/java/com/re_teraction/backend/infra/serializer/PasswordSerializer.java
+++ src/main/java/com/re_teraction/backend/infra/serializer/PasswordSerializer.java
@@ -3,14 +3,14 @@ package com.re_teraction.backend.infra.serializer;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import com.re_teraction.backend.domain.user.vo.Email;
+import com.re_teraction.backend.domain.user.vo.Password;
 import java.io.IOException;
 
-public class PasswordSerializer extends StdSerializer<Email> {
-    public PasswordSerializer() { super(Email.class); }
+public class PasswordSerializer extends StdSerializer<Password> {
+    public PasswordSerializer() { super(Password.class); }
 
     @Override
-    public void serialize(Email value, JsonGenerator gen, SerializerProvider provider)
+    public void serialize(Password value, JsonGenerator gen, SerializerProvider provider)
             throws IOException {
         gen.writeString(value.getValue());
     }
