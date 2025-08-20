cbcaa1ff1c32c17aaedcf849888b82b84b79257f
KNU-K <knukang334@gmail.com>
2025-08-20 16:23:08 +0900

    feat(project): ProjectCategory 를 Mapping 하는 ProjectMapper 추가

---
+++ src/main/java/com/re_teraction/backend/domain/project/service/ProjectMapper.java
@@ -0,0 +1,15 @@
+package com.re_teraction.backend.domain.project.service;
+
+import com.re_teraction.backend.domain.project.vo.ProjectCategory;
+import com.re_teraction.backend.global.annotation.DomainService;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@DomainService
+public class ProjectMapper {
+    public Set<ProjectCategory> toProjectCategories(Set<String> categories) {
+        return categories.stream()
+                .map(ProjectCategory::fromValue)
+                .collect(Collectors.toSet());
+    }
+}
