6ad634a7a9ad972a3d18ddf8c9803a1492d21457
KNU-K <knukang334@gmail.com>
2025-08-21 04:27:04 +0900

    feat : EnrollmentJpaRepository 추가

---
+++ src/main/java/com/re_teraction/backend/domain/enrollment/EnrollmentJpaRepository.java
@@ -0,0 +1,7 @@
+package com.re_teraction.backend.domain.enrollment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnrollmentJpaRepository extends JpaRepository<EnrollmentJpaEntity, Long> {
+
+}
