77245475d870d74eb0149a93e9825e6b5048d437
KNU-K <knukang334@gmail.com>
2025-08-21 04:26:21 +0900

    feat(enroll) : 수강 신청 Entity 추가

---
+++ src/main/java/com/re_teraction/backend/domain/enrollment/EnrollmentJpaEntity.java
@@ -0,0 +1,39 @@
+package com.re_teraction.backend.domain.enrollment;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "enrollments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class EnrollmentJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Embedded
+    private Applicant applicant;
+
+    @Enumerated(EnumType.STRING)
+    private Program program;
+
+    @Builder
+    public EnrollmentJpaEntity(Applicant applicant, Program program) {
+        this.applicant = applicant;
+        this.program = program;
+    }
+}
