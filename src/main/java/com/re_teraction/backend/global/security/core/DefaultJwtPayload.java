f1b979b6d76b20bfd1758dbcb3b95f233e5dc46e
KNU-K <knukang334@gmail.com>
2025-08-20 22:08:35 +0900

    feat(security): Introduce JwtPayload and DefaultJwtPayload classes for JWT handling

---
+++ src/main/java/com/re_teraction/backend/global/security/core/DefaultJwtPayload.java
@@ -0,0 +1,17 @@
+package com.re_teraction.backend.global.security.core;
+
+import io.jsonwebtoken.Claims;
+
+public class DefaultJwtPayload extends JwtPayload {
+    protected DefaultJwtPayload(String subject, Claims claims) {
+        super(subject, claims);
+    }
+    @Override
+    public Claims getClaims() {
+        return claims;
+    }
+    @Override
+    public String getSubject() {
+        return subject;
+    }
+}
