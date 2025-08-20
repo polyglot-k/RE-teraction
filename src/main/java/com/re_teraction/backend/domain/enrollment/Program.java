ebc940b9b888ec2201a22bb8a49875d9e16fb218
KNU-K <knukang334@gmail.com>
2025-08-21 04:25:25 +0900

    feat(enroll) : 수강 신청할 프로그램 Enum 으로 구성

---
+++ src/main/java/com/re_teraction/backend/domain/enrollment/Program.java
@@ -0,0 +1,36 @@
+package com.re_teraction.backend.domain.enrollment;
+
+import com.re_teraction.backend.global.exception.BusinessException;
+import com.re_teraction.backend.global.exception.ErrorCode;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Program {
+    SCHOLASTIC("Scholastic"),
+    INDUSTRY("Industry");
+
+    private static final Map<String, Program> BY_VALUE = new HashMap<>();
+
+    static {
+        for (Program e : values()) {
+            BY_VALUE.put(e.value, e);
+        }
+    }
+
+    private final String value;
+
+    public static Program fromValue(String value) {
+        Program program = BY_VALUE.get(value);
+        if (program == null) {
+            throw new BusinessException(ErrorCode.INVALID_PROGRAM);
+        }
+        return program;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+}
