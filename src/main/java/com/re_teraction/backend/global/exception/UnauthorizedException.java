c13615189678926effc409dbc5b0ce4b52fad6ea
KNU-K <knukang334@gmail.com>
2025-08-21 00:14:47 +0900

    feat(exception): Add UnauthorizedException for handling unauthorized access

---
+++ src/main/java/com/re_teraction/backend/global/exception/UnauthorizedException.java
@@ -0,0 +1,12 @@
+package com.re_teraction.backend.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
