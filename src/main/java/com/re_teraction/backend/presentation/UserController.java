f323de00e2d1266ea564c9edca005ba8d74f544d
KNU-K <knukang334@gmail.com>
2025-08-20 15:17:55 +0900

    feat(user): 개선된 사용자 생성 API 응답 URI 생성 방식

--- src/main/java/com/re_teraction/backend/presentation/UserController.java
+++ src/main/java/com/re_teraction/backend/presentation/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -31,7 +32,10 @@ public class UserController {
     @PostMapping()
     public ResponseEntity<? extends ApiResponse<?>> create(@RequestBody CreateUserCommand cmd){
         UserResponse response = userApplicationService.createUser(cmd);
-        URI location = URI.create("/api/v1/users/" + response.id());
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
         return ResponseEntity
                 .created(location)
                 .body(ApiResponseFactory.success(response, "회원가입 성공"));
