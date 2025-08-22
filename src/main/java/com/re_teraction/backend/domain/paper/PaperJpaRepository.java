8efb4c4f8653c8f9fa163a23ce74aeae78ba1c2b
KNU-K <knukang334@gmail.com>
2025-08-23 02:51:26 +0900

    chore: 사용안되는 객체 제거

--- src/main/java/com/re_teraction/backend/domain/paper/PaperJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.re_teraction.backend.domain.paper;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaperJpaRepository extends JpaRepository<PaperJpaEntity, Long> {
-
-}
