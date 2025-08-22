306669d4b002bc4ba4125d897610bee81fda2724
KNU-K <knukang334@gmail.com>
2025-08-23 02:46:37 +0900

    fix : 불필요한 메소드 기반 쿼리 제거

--- src/main/java/com/re_teraction/backend/domain/project/repo/ProjectJpaRepository.java
+++ src/main/java/com/re_teraction/backend/domain/project/repo/ProjectJpaRepository.java
@@ -1,11 +1,9 @@
 package com.re_teraction.backend.domain.project.repo;
 
 import com.re_teraction.backend.domain.project.entity.ProjectJpaEntity;
-import com.re_teraction.backend.domain.project.vo.ProjectCategory;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectJpaRepository extends JpaRepository<ProjectJpaEntity, Long> {
+public interface ProjectJpaRepository extends JpaRepository<ProjectJpaEntity, Long>,
+        ProjectJpaRepositoryCustom {
 
-    List<ProjectJpaEntity> findByCategoriesContaining(ProjectCategory category);
 }
