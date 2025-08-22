a14807df87beb3ad199e13a6b3456a3f3cb80be4
KNU-K <knukang334@gmail.com>
2025-08-23 02:52:42 +0900

    refactor: join 에 따른 paper, project applicationService DTO 설정 변경

--- src/main/java/com/re_teraction/backend/application/paper/PaperApplicationService.java
+++ src/main/java/com/re_teraction/backend/application/paper/PaperApplicationService.java
@@ -1,9 +1,9 @@
 package com.re_teraction.backend.application.paper;
 
 import com.re_teraction.backend.application.paper.dto.CreatePaperCommand;
-import com.re_teraction.backend.application.paper.dto.PaperResponse;
-import com.re_teraction.backend.domain.paper.PaperJpaEntity;
-import com.re_teraction.backend.domain.paper.PaperJpaRepository;
+import com.re_teraction.backend.application.paper.dto.PaperWithThumbnailResponse;
+import com.re_teraction.backend.domain.paper.entity.PaperJpaEntity;
+import com.re_teraction.backend.domain.paper.repo.PaperJpaRepository;
 import com.re_teraction.backend.global.annotation.ApplicationService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -17,21 +17,21 @@ public class PaperApplicationService {
     private final PaperJpaRepository paperJpaRepository;
 
     @Transactional
-    public PaperResponse createPaper(Long userId, CreatePaperCommand command) {
-        PaperJpaEntity paper = paperJpaRepository.save(toEntity(userId, command));
-        return PaperResponse.from(paper);
+    public void createPaper(Long userId, CreatePaperCommand command) {
+        paperJpaRepository.save(toEntity(userId, command));
     }
 
     @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
-    public List<PaperResponse> getPapers() {
-        return paperJpaRepository.findAll()
+    public List<PaperWithThumbnailResponse> getPapers() {
+        //thumbnail id 추가 join
+        return paperJpaRepository.findAllWithThumbnail()
                 .stream()
-                .map(PaperResponse::from)
+                .map(PaperWithThumbnailResponse::from)
                 .toList();
     }
 
     private PaperJpaEntity toEntity(Long userId, CreatePaperCommand command) {
-        return PaperJpaEntity.of(command.thumbnailUrl(), command.title(), userId);
+        return PaperJpaEntity.of(command.thumbnailId(), command.title(), userId);
     }
 
     public void deletePaper(Long paperId) {
--- src/main/java/com/re_teraction/backend/application/project/ProjectApplicationService.java
+++ src/main/java/com/re_teraction/backend/application/project/ProjectApplicationService.java
@@ -17,18 +17,25 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ProjectApplicationService {
 
+    private final ProjectCategoryEventPublisher categoryEventPublisher;
+
     private final ProjectJpaRepository projectJpaRepository;
     private final ProjectMapper projectMapper;
 
     @Transactional
-    public ProjectResponse createProject(Long userId, CreateProjectCommand cmd) {
-        ProjectJpaEntity project = projectJpaRepository.save(toEntity(userId, cmd));
-        return ProjectResponse.from(project);
+    public void createProject(Long userId, CreateProjectCommand cmd) {
+        Long projectId = projectJpaRepository.save(toEntity(userId, cmd)).getId();
+
+        // 카테고리 생성
+        categoryEventPublisher.publishProjectCategoryCreated(
+                projectId,
+                Set.copyOf(cmd.categories())
+        );
     }
 
     @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public List<ProjectResponse> getProjects() {
-        return projectJpaRepository.findAll()
+        return projectJpaRepository.findAllWithThumbnail()
                 .stream()
                 .map(ProjectResponse::from)
                 .toList();
@@ -36,15 +43,15 @@ public class ProjectApplicationService {
 
     @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public List<ProjectResponse> getProjectByCategory(String category) {
-        return projectJpaRepository.findByCategoriesContaining(ProjectCategory.fromValue(category))
+        ProjectCategory projectCategory = ProjectCategory.fromValue(category);
+        return projectJpaRepository.findAllWithThumbnailByCategoriesContaining(projectCategory)
                 .stream()
                 .map(ProjectResponse::from)
                 .toList();
     }
 
     private ProjectJpaEntity toEntity(Long userId, CreateProjectCommand cmd) {
-        Set<ProjectCategory> categories = projectMapper.toProjectCategories(cmd.categories());
-        return ProjectJpaEntity.of(cmd.thumbnailUrl(), cmd.title(), categories, userId);
+        return ProjectJpaEntity.of(cmd.thumbnailId(), cmd.title(), userId);
     }
 
     public void deleteProject(Long projectId) {
