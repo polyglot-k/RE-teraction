e2dc9698f6b8a71a81eac94e14c354e6e34da785
KNU-K <knukang334@gmail.com>
2025-08-23 03:00:09 +0900

    chore: 변수명 오타 수정

--- src/main/java/com/re_teraction/backend/application/thumbnail/dto/ThumbnailResponse.java
+++ src/main/java/com/re_teraction/backend/application/thumbnail/dto/ThumbnailResponse.java
@@ -2,9 +2,9 @@ package com.re_teraction.backend.application.thumbnail.dto;
 
 import java.net.URI;
 
-public record ThumbnailResponse(Long ThumbnailId, URI uri) {
+public record ThumbnailResponse(Long thumbnailId, URI uri) {
 
-    public static ThumbnailResponse of(Long ThumbnailId, URI uri) {
-        return new ThumbnailResponse(ThumbnailId, uri);
+    public static ThumbnailResponse of(Long thumbnailId, URI uri) {
+        return new ThumbnailResponse(thumbnailId, uri);
     }
 }
